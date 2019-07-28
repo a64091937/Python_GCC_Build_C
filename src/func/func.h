cb82613f7833255c74a665e54a69e1df2ff57e6f
a64091937 <643717213@qq.com>
2019-07-28 19:10:46 +0800

    一个使用Python和gcc编译C语言文件的项目

---
+++ src/func/func.h
@@ -0,0 +1,14 @@
+/*	
+ Copyright © 2019-2019, Zhang Hongda, a64091937. All Rights Reserved.
+ Description:A Standard STM32 Project
+ Author:Zhang Hongda
+ Creation time:2019-7-28
+ */
+ 
+#ifndef FUNC_H
+#define FUNC_H
+
+int a[3] = {0,1,2};
+int add(int a, int b);
+
+#endif
