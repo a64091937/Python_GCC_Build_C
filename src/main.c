1ec023f4c50bc342a1a786775ac49324b624d028
a64091937 <643717213@qq.com>
2019-07-28 19:10:46 +0800

    一个使用Python和gcc编译C语言文件的项目

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/*	
+ Copyright © 2019-2019, Zhang Hongda, a64091937. All Rights Reserved.
+ Description:A Standard STM32 Project
+ Author:Zhang Hongda
+ Creation time:2019-7-28
+ */
+#include <stdio.h>
+#include "func/func.h"
+
+int main() 
+{
+    int c = add(a[1],a[2]);
+    printf("Hello World!%d", c);
+    return 0;
+}
