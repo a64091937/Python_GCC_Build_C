cddb22774dd775940d69f78f41b568352dfcaeac
a64091937 <643717213@qq.com>
2019-08-04 23:23:10 +0800

    完善文件头

--- src/func/func.h
+++ src/func/func.h
@@ -1,6 +1,6 @@
 /*	
  Copyright © 2019-2019, Zhang Hongda, a64091937. All Rights Reserved.
- Description:A Standard STM32 Project
+ Description:A Standard Build C Project
  Author:Zhang Hongda
  Creation time:2019-7-28
  */
